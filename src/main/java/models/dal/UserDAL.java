1fc54bf67112e30793cc3b801797bbd0f1d0048f
Nikolajus Afanasenka <nikolajus.a@gmail.com>
2018-07-01 11:00:01 +0300

    Path problems resolved.

---
+++ src/main/java/models/dal/UserDAL.java
@@ -0,0 +1,11 @@
+package models.dal;
+
+public class UserDAL {
+
+	public Integer userId;
+	public String name;
+	public String password;
+	public String eMail;
+	public Integer accessLevel;
+
+}
---
+++ src/main/java/models/dto/DTO.java
@@ -0,0 +1,8 @@
+package models.dto;
+
+public class DTO {
+
+	public boolean success;
+	public String message;
+
+}
