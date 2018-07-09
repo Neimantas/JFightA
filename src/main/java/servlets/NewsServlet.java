93ba292b9b846a2dd3a74d8d69bc0554127b060a
CrossPC\Karolis <karolis.vas@gmail.com>
2018-07-09 15:56:08 +0300

    buttons...

--- src/main/java/servlets/NewsServlet.java
+++ src/main/java/servlets/NewsServlet.java
@@ -10,39 +10,52 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet(urlPatterns = "/News")
 public class NewsServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public NewsServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		//response.getWriter().append("Served at: ").append(request.getContextPath());
-	
-//		String button = request.getParameter("btn3");
-//		if ("btn3".equals(button)) {
-//			 request.getRequestDispatcher("/JFight/aaa").forward(request, response);
-//			 }
-//		else {
-			request.getRequestDispatcher("News.jsp").forward(request, response);
-//		}
-		doPost(request, response);
-		
+	public NewsServlet() {
+		super();
+		// TODO Auto-generated constructor stub
 	}
+
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		// response.getWriter().append("Served at: ").append(request.getContextPath());
+
+		String param = request.getParameter("button");
+
+		if (param != null) {
+
+			if (param.equals("logout")) {
+
+				request.setAttribute("button", param);
+				request.getRequestDispatcher("index.jsp").forward(request, response);
+			}
+
+		} else {
+			request.getRequestDispatcher("News.jsp").forward(request, response);
+		}
+
+		// }
+		// doPost(request, response);
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 
 		// TODO Auto-generated method stub
-		//doGet(request, response);
+		// doGet(request, response);
 
 	}
 
