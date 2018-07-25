dd7f2f42bb682696f8970e3c65bc03453fb82a15
Kaciukas <stash1990@gmail.comj>
2018-07-25 11:03:24 +0300

    All in one place

--- src/main/java/services/ILogger.java
+++ src/main/java/services/ILogger.java
@@ -8,6 +8,6 @@ import models.dto.ObjectDTO;
 public interface ILogger {
 
 	ObjectDTO<FightDataDAL> logFightData(int fightId, int userIdA, int userIdB);
-	ListDTO<FightDataDAL> getLogs(int userIdA, int userIdB);
+	ListDTO<String> getLogs(int userIdA, int userIdB);
 
 }
